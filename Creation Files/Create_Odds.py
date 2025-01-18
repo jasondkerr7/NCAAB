1d6fe7c432cef2c227f719601e55be06559fdfe6
jasondkerr7 <164694209+jasondkerr7@users.noreply.github.com>
2025-01-17 19:02:48 -0500

    Create_Odds.py
    import the required packages

--- Creation Files/Create_Odds.py
+++ Creation Files/Create_Odds.py
@@ -4,6 +4,12 @@
 
 ## -- Import Required Packages -- ##
 
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
 import smtplib
 from bs4.element import Comment
 import re
@@ -11,6 +17,8 @@ import requests
 from pydomo import Domo
 import openpyxl
 import datetime
+from datetime import datetime, date, timedelta
+from datetime import time as datetime_time
 import pandas as pd
 import pickle
 import time
@@ -18,6 +26,8 @@ import numpy as np
 from io import StringIO
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import statsmodels.api as SM
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 from calendar import monthrange
 from bs4 import BeautifulSoup
 import math
@@ -28,6 +38,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os
+import glob
+import urllib
+import gzip
+import json
+import lxml
 
 # -- GOOGLE CONNECTION -- #
 # Prepare auth json for google connection
