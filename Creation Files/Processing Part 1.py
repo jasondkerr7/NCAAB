faefd4bcf281d227370ac217d7fa7610087dfe7a
jasondkerr7 <164694209+jasondkerr7@users.noreply.github.com>
2025-02-13 20:37:56 -0500

    Update and rename Processing.py to Processing Part 1.py

--- Creation Files/Processing Part 1.py
+++ Creation Files/Processing Part 1.py
@@ -307,68 +307,21 @@ oddsv6 = pd.merge(oddsv5, oppsosref, on=['Date','Opp'], how='left')
 # Reset Memory
 del oddsv5
 
-print('VOP')
-# -- Variance of Play --
-### Variance of Play defined as Spread in Wins minus Spread in Losses ###
-# Temp variables
-oddsv6['tempSpreadInLoss'] = (oddsv6['ResultDummy'] == 0)*(oddsv6['Spread'])
-oddsv6['tempSpreadInWin'] = (oddsv6['ResultDummy'] > 0)*(oddsv6['Spread'])
-
-# Cumulatively Sum the Temp Variables
-oddsv6['VOPW'] = (oddsv6.groupby(['Season','Team'])['tempSpreadInWin'].cumsum() -\
-                                                        oddsv6['tempSpreadInWin'])/oddsv6['G']
-oddsv6['VOPL'] = (oddsv6.groupby(['Season','Team'])['tempSpreadInLoss'].cumsum() -\
-                                                        oddsv6['tempSpreadInLoss'])/oddsv6['G']
-oddsv6['VOP'] = oddsv6['VOPW']-oddsv6['VOPL']
-# Normalize all three
-for var in ['VOPW','VOPL','VOP']:
-    mn = oddsv6[var].mean()
-    std = oddsv6[var].std()
-    oddsv6[var] = (oddsv6[var]-mn)/std
-# Remove Temp variables
-oddsv7 = oddsv6.drop(['tempSpreadInLoss','tempSpreadInWin'],axis=1)
-# Reset Memory
-del oddsv6
-
-# Opponent VOP
-oppvopref = oddsv7[['Date','Team','VOPW','VOPL','VOP']].copy().rename(columns={'Team':'Opp',
-                                                                                'VOP':'OppVOP',
-                                                                                'VOPW':'OppVOPW',
-                                                                                'VOPL':'OppVOPL'})
-oddsv8 = pd.merge(oddsv7, oppvopref, on=['Date','Opp'], how='left')
-oddsv8['VOPsum'] = oddsv8['VOP'] + oddsv8['OppVOP']
-# Reset Memory
-del oddsv7
-
-print('CGWR')
-# -- Close Game Weighted Record -- 
-# Sort Values
-oddsv8 = oddsv8.sort_values('Date')
-
-# Results weighted by Closeness of Games to determine clutch value
-oddsv8['tempCGWR'] = np.maximum(0.1, (1-((abs(oddsv8['MOV'])/100)**3)*3000))*(oddsv8['MOV']/oddsv8['MOV'].abs())
-oddsv8['CGWR'] = oddsv8.groupby(['Season','Team'])['tempCGWR'].cumsum() - oddsv8['tempCGWR']
-
-# Drop Temp
-oddsv9 = oddsv8.drop('tempCGWR',axis=1)
-# Reset Memory
-del oddsv8
-
 ##################################################
 ###### End Processing ############################
 ##################################################
 
 print('File Creation')
 # Test File Creation #
-creation_name = 'Processed Stats 2021-2024'
-oddsv9.to_csv('saved_file.csv',index=False)
+creation_name = 'Incomplete - Processed Stats 2021-2024'
+oddsv6.to_csv('saved_file.csv',index=False)
 
 # Upload File
 returned_fields="id, name, mimeType, webViewLink, exportLinks, parents"
 file_metadata = {'name': creation_name+'.csv'}
 media = MediaFileUpload('saved_file.csv',
                         mimetype='text/csv')
-file = ggl_drive.files().update(fileId='17p3ZBuoFeYSj4E64pRuLUJBL7LpSvfin',
+file = ggl_drive.files().update(fileId='1JSa-KqrY3TnE9tyiUxoznbHPwL2JimqT',
                                 body=file_metadata, 
                                 media_body=media,
                               fields=returned_fields).execute()
